99f7ade3eed40a97a909ebde0224ac1819a4f9c9
MarianaAleman <91183781+MarianaAleman@users.noreply.github.com>
2021-09-22 10:31:54 +0100

    Implementations of Interfaces

---
+++ RentACar/src/algorithmsandconstructs/BookingSystem.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package algorithmsandconstructs;
+
+import algorithmsandconstructs.BookingSystemInterface;
+import algorithmsandconstructs.RentACarInterface;
+import java.io.BufferedReader;
+import java.io.IOException;
+
+/**
+ *
+ * @author maria
+ */
+public class BookingSystem implements BookingSystemInterface {
+
+    @Override
+    public RentACarInterface setupRentACar(BufferedReader in) throws IOException {
+    
+        
+    }
+    
+}
